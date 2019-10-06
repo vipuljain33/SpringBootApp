dad738f1420cb664632a7a57329087e31b1b14d6
vipul jain <vipuljain33@gmail.com>
2019-10-07 00:12:29 +0530

    Filtering Implementation

---
+++ src/main/java/com/example/SpringBootApp/SpringBootApp/filter/FilteringController.java
@@ -0,0 +1,26 @@
+package com.example.SpringBootApp.SpringBootApp.filter;
+
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilteringController {
+
+
+    @GetMapping("/filtering")
+    public MappingJacksonValue retrieveSomeBean()
+    {
+       SomeBean someBean = new SomeBean("value1", "value2", "value3");
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field3");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+}
