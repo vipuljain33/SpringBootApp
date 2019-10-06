7593416fe83db76556e57aeaf9c8c0359de11a2a
vipul jain <vipuljain33@gmail.com>
2019-10-06 15:13:12 +0530

    Returning user created in response

--- src/main/java/com/example/SpringBootApp/SpringBootApp/user/UserController.java
+++ src/main/java/com/example/SpringBootApp/SpringBootApp/user/UserController.java
@@ -2,8 +2,11 @@ package com.example.SpringBootApp.SpringBootApp.user;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -37,9 +40,13 @@ public class UserController {
     //output - created and uri of created user
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user)
+    public ResponseEntity<Object> createUser(@RequestBody User user)
     {
         User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 
