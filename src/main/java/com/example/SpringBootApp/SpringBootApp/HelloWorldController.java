3f9ac72c6e9d1d3e9677de97d38483c9c1240c7a
vipul jain <vipuljain33@gmail.com>
2019-10-06 13:21:37 +0530

    Basic Spring Boot Concepts

---
+++ src/main/java/com/example/SpringBootApp/SpringBootApp/HelloWorldController.java
@@ -0,0 +1,30 @@
+package com.example.SpringBootApp.SpringBootApp;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello-world" )
+    public String helloWorld()
+    {
+        return "Hello World";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello-world-bean" )
+    public HelloWorldBean helloWorldBean()
+    {
+        return new HelloWorldBean("Hello World");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello-world/path-variable/{name}" )
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name)
+    {
+        return new HelloWorldBean("Hello World, "+ name);
+    }
+}
